87ef94dddde8c6937f883b2e5e00710835f259f4
Johall <johnalexander.w.hall@gmail.com>
2015-12-12 14:00:36 -0500

    added functionality of start button / moved all toggle info into rpsgame

--- js/rpsgame.js
+++ js/rpsgame.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+
   var rpsValue = ['rock', 'paper', 'scissors'];
 
   var computerScore = 0;
@@ -9,49 +10,72 @@ $( document ).ready(function() {
   var roundCount = 0;
 
 
- $(".btn-info").on("click", function() {
-  
-  roundCount++;
-  $('#roundCount').html(roundCount);
+  function bindControls() {
 
-  var computerChoice = rpsValue[Math.floor(Math.random() * rpsValue.length)];
-  var userChoice = ($(this).attr("data-throw"));
+    $(".btn-info").on("click", function() {
 
-  $('#userChoice').html(" " + userChoice);
-  $('#computerChoice').html(" " + computerChoice);
+      roundCount++;
+      $('#roundCount').html(roundCount);
+
+      var computerChoice = rpsValue[Math.floor(Math.random() * rpsValue.length)];
+      var userChoice = ($(this).attr("data-throw"));
+
+      $('#userChoice').html(" " + userChoice);
+      $('#computerChoice').html(" " + computerChoice);
 
 
-  if (computerChoice === userChoice) {
-   $('#endGameMessage').html('\'twas a tie');
-   ties++;
-   $('#ties').html(ties);
- } 
- else if (computerChoice === 'rock' && userChoice === 'scissors') {
-   $('#endGameMessage').html('Computer wins');
-   computerScore++;
-   $('#computerScore').html(computerScore);
- } 
- else if (computerChoice === 'scissors' && userChoice === 'paper') {
-   $('#endGameMessage').html('Computer Wins');
-   computerScore++;
-   $('#computerScore').html(computerScore);
- } 
- else if (computerChoice === 'paper' && userChoice === 'rock') {
-   $('#endGameMessage').html('Computer Wins');
-   computerScore++;
-   $('#computerScore').html(computerScore);
- } 
- else {
-   $('#endGameMessage').html("User Wins");
-   userScore++;
-   $('#userScore').html(userScore);
- }
+      if (computerChoice === userChoice) {
+       $('#endGameMessage').html('\'twas a tie');
+       ties++;
+       $('#ties').html(ties);
+     } 
+     else if (computerChoice === 'rock' && userChoice === 'scissors') {
+       $('#endGameMessage').html('Computer wins');
+       computerScore++;
+       $('#computerScore').html(computerScore);
+     } 
+     else if (computerChoice === 'scissors' && userChoice === 'paper') {
+       $('#endGameMessage').html('Computer Wins');
+       computerScore++;
+       $('#computerScore').html(computerScore);
+     } 
+     else if (computerChoice === 'paper' && userChoice === 'rock') {
+       $('#endGameMessage').html('Computer Wins');
+       computerScore++;
+       $('#computerScore').html(computerScore);
+     } 
+     else {
+       $('#endGameMessage').html("User Wins");
+       userScore++;
+       $('#userScore').html(userScore);
+     }
+   });
+}
 
+//code used to toggle Game on and off
+
+$("#toggleBtn").on("click", function() {
+  if($(this).attr("data-status") === "off") {
+
+    $(this)
+    .html("Game on")
+    .addClass("btn-success")
+    .removeClass("btn-danger")
+    .attr("data-status", "on");
+    bindControls();
+
+
+  } else {
+    $(this)
+    .html("Game off")
+    .addClass("btn-danger")
+    .removeClass("btn-success")
+    .attr("data-status", "off");
+    $(".btn-info").off();
+  }
 });
 
+//animations
 
 
 });
-   
-   
-   
--- js/togglestate.js
+++ js/togglestate.js
@@ -1,25 +1,5 @@
 $( document ).ready(function() {
 
-   $("#toggleBtn").on("click", function() {
-    if($(this).attr("data-status") === "on") {
-
-      $(this)
-        .html("Play on")
-        .addClass("btn-success")
-        .removeClass("btn-danger")
-        .attr("data-status", "off");
-
-
-    } else {
-      $(this)
-        .html("Game off")
-        .addClass("btn-danger")
-        .removeClass("btn-success")
-        .attr("data-status", "on");
-        $('.btn-info').off();
-    }
-  });
-
 
 
 });
